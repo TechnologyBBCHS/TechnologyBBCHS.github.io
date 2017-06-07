c4c3c610b905c62497f57f4ee25c70292376182f
TechnologyBBCHS <helpbox@bbchs.org>
2017-06-07 13:17:06 -0500

    f

--- assets/js/iframe.js
+++ assets/js/iframe.js
@@ -1,4 +1,6 @@
-  $('iframe.iframe_auto').iframeAutoHeight({
-	minHeight: 240, // Sets the iframe height to this value if the calculated value is less
-  heightOffset: 50 // Optionally add some buffer to the bottom
-  });
+$(document).ready(function() {
+	$('iframe.iframe_auto').iframeAutoHeight({
+		minHeight: 240, // Sets the iframe height to this value if the calculated value is less
+		heightOffset: 50 // Optionally add some buffer to the bottom
+	});
+});
